f1739787bb5479a72fc4c85c9460ab0553cb7b18
Joachim Baur <post@joachimbaur.de>
2026-05-21 20:44:00 +0200

    Added sound code & more characters

--- src/output_fill_matrix_spiral.cpp
+++ src/output_fill_matrix_spiral.cpp
@@ -1,4 +1,5 @@
 #include "output_fill_matrix_spiral.h"
+#include "audio.h"
 #include "display.h"
 #include "main.h"
 #include <Arduino.h>
@@ -18,72 +19,73 @@ void output_fill_matrix_spiral()
 void output_fill_matrix_spiral(bool init)
 {
     static uint8_t round = 0;
-    static uint8_t mode = 1; // 1 = fill, 0 = clear
+    static uint8_t mode = mode_fill;
     static uint8_t direction = 0;
 
     if (init) {
         round = 0;
-        mode = 1;
-        direction = 0;
+        mode = mode_fill;
+        direction = dir_right;
         x = -1;
         y = 0;
     }
 
     if (countdown == 0) {
-        if (mode == 1) {
-            // fill
+        if (mode == mode_fill) {
             if (x == round && y == round + 1) {
-                direction = 0;
+                direction = dir_right;
+                playAudio(random(24 + random(24)), HALF);
                 round++;
             } else if (x == 7 - round && y == round) {
-                direction = 1;
+                direction = dir_up;
             } else if (x == 7 - round && y == 7 - round) {
-                direction = 2;
+                direction = dir_left;
             } else if (x == round && y == 7 - round) {
-                direction = 3;
+                direction = dir_down;
             }
 
         } else {
             // clear
             if (x == round && y == round) {
-                direction = 0;
+                direction = dir_right;
             } else if (x == 7 - round && y == round) {
-                direction = 1;
+                direction = dir_up;
             } else if (x == 7 - round && y == 7 - round) {
-                direction = 2;
+                direction = dir_left;
             } else if (x == round - 1 && y == 7 - round) {
-                direction = 3;
+                direction = dir_down;
+                playAudio(random(24 + random(24)), HALF);
                 round--;
             }
         }
 
         switch (direction) {
-        case 0:
+        case dir_right:
             x++;
             break; // to the right
-        case 1:
+        case dir_up:
             y++;
             break; // upwards
-        case 2:
+        case dir_left:
             x--;
             break; // to the left
-        case 3:
+        case dir_down:
             y--;
             break; // downwards
         }
 
         matrixSetPixel(x, y, mode);
 
-        if (mode == 1 && x == 3 && y == 4) { // last pixel activ - switch to turn off
-            mode = 0;
-            direction = 0;
+        if (mode == mode_fill && x == 3 && y == 4) { // last pixel activ - switch to turn off
+            mode = mode_clear;
+            direction = dir_right;
             x--;
             y--;
         }
 
-        if (mode == 0 && x == 0 && y == 7) { // last pixel clear - refill again
-            mode = 1;
-            direction = 0;
+        if (mode == mode_clear && x == 0 && y == 7) { // last pixel clear - refill again
+            mode = mode_fill;
+            direction = dir_right;
             x = -1;
             y = 0;
         }
